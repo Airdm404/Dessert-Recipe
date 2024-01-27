5a80fd28b0813533955af9166ed2bdef8646d17f
Edem Ahorlu <eka13@duke.edu>
2024-01-27 03:20:37 -0500

    Initial Commit

---
+++ Dessert Recipe/Dessert_RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dessert_RecipeApp.swift
+//  Dessert Recipe
+//
+//  Created by Edem Ahorlu on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Dessert_RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
